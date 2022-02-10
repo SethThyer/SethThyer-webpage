e30a75b6acbc76f54db580364798627e036c30ff
SethThyer <seththyer23@gmail.com>
2022-02-11 07:23:01 +1100

    changed email

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function MyApp() {
           <a href="/books">🔖 Books</a>
         </h3>
         <h3>
-          <a href="mailto:seththyer23@gmail.com?subject=Sent%20From%20SethThyersPage">
+          <a href="mailto:main@seththyer.com?subject=Sent%20From%20SethThyersPage">
             📯 Email
           </a>
         </h3>
@@ -33,13 +33,13 @@ function MyApp() {
       <h2>⛵ Where I Reside ⚓</h2>
       <center>
         <h3 className="inline-c">
-          <a href="https://github.com/SethThyer">
+          <a target="_blank" href="https://github.com/SethThyer">
             <Image src="/images/git-icon.svg" width="25rem" height="25rem" />
             &nbsp;Github
           </a>
         </h3>
         <h3 className="inline-c">
-          <a href="https://twitter.com/Seth06098372">
+          <a target="_blank" href="https://twitter.com/Seth06098372">
             <Image
               src="/images/twitter-icon.svg"
               width="25rem"
@@ -49,7 +49,10 @@ function MyApp() {
           </a>
         </h3>
         <h3 className="inline-c">
-          <a href="https://open.spotify.com/user/qwr2j3sfc6quajm51dtz54sde?si=c91e27cd71b4497a&nd=1">
+          <a
+            target="_blank"
+            href="https://open.spotify.com/user/qwr2j3sfc6quajm51dtz54sde?si=c91e27cd71b4497a&nd=1"
+          >
             <Image
               src="/images/spotify-icon.svg"
               width="25rem"
@@ -61,7 +64,7 @@ function MyApp() {
       </center>
       <h2>🌐 Some Cool Sites 📰</h2>
       <center className="gif">
-        <a href="https://larbs.xyz">
+        <a target="_blank" href="https://larbs.xyz">
           <Image
             alt="LARBS"
             src="https://larbs.xyz/pix/larbs.gif"
@@ -69,7 +72,7 @@ function MyApp() {
             height="31px"
           />
         </a>
-        <a href="https://yesterweb.org/no-to-web3/">
+        <a target="_blank" href="https://yesterweb.org/no-to-web3/">
           <Image
             alt="NO WEB3"
             src="https://auzziejay.com/images/noweb32.gif"
@@ -93,14 +96,14 @@ function MyApp() {
             height="31px"
           />
         </a>
-        <a href="https://openbooks.neocities.org">
+        <a target="_blank" href="https://openbooks.neocities.org">
           <Image
             src="https://openbooks.neocities.org/img/88x31.gif"
             width="88px"
             height="31px"
           />
         </a>
-        <a href="https://seththyer.com/">
+        <a target="_blank" href="https://seththyer.com/">
           <Image src="/images/web-button-gif.gif" width="88px" height="31px" />
         </a>
       </center>
